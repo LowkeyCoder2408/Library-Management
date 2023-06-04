30322c9afb66747777614e3875541d456933cb7f
Đỗ Kim Lâm <125809812+LowkeyCoder2408@users.noreply.github.com>
2023-06-04 19:13:47 +0700

    BOOK.h

--- LIBRARY MANAGEMENT SYSTEM/include/BOOK.h
+++ LIBRARY MANAGEMENT SYSTEM/include/BOOK.h
@@ -1,12 +1,11 @@
-#ifndef BOOK_H
-#define BOOK_H
 #include <iostream>
 #include "DATE.h"
+#ifndef BOOK_H
+#define BOOK_H
 
 using namespace std;
 
-class BOOK
-{
+class BOOK {
 	private:
 		string idBook;
 		string title;
@@ -18,32 +17,32 @@ class BOOK
 		int total;
 		int remain;
 	public:
-		BOOK ();
-		BOOK (string, string, string, string, int, string, double, int, int);
-		void setIdBook (string);
-		string getIdBook ();
-		void setTitle (string);
-		string getTitle ();
-		void setAuthor (string);
-		string getAuthor ();
-		void setPublisher (string);
-		string getPublisher ();
-		void setPublishYear (int);
-		int getPublishYear ();
-		void  setGenre (string);
-		string getGenre ();
-		void setCoverPrice (double);
-		double getCoverPrice ();
-		void setTotal (int);
-		int getTotal ();
-		void setRemain (int);
-		int getRemain ();
+		BOOK();
+		BOOK(string, string, string, string, int, string, double, int, int);
+		void setIdBook(string);
+		string getIdBook();
+		void setTitle(string);
+		string getTitle();
+		void setAuthor(string);
+		string getAuthor();
+		void setPublisher(string);
+		string getPublisher();
+		void setPublishYear(int);
+		int getPublishYear();
+		void  setGenre(string);
+		string getGenre();
+		void setCoverPrice(double);
+		double getCoverPrice();
+		void setTotal(int);
+		int getTotal();
+		void setRemain(int);
+		int getRemain();
 		void output();
-		friend istream &operator>> (istream &in, BOOK &book);
-		friend ostream &operator<< (ostream &out, BOOK book);
+		friend istream &operator>>(istream &in, BOOK &book);
+		friend ostream &operator<<(ostream &out, BOOK book);
 		void readFileABook(ifstream &inFile);
-		void exportFileABook (ofstream &outFile);
-		~BOOK ();
+		void exportFileABook(ofstream &outFile);
+		~BOOK();
 };
 
 #endif
