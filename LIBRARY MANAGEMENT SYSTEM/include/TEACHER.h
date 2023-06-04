7435975925474314da523a7d1228b5e2140ae04c
Đỗ Kim Lâm <125809812+LowkeyCoder2408@users.noreply.github.com>
2023-06-04 19:25:41 +0700

    Update TEACHER.h

--- LIBRARY MANAGEMENT SYSTEM/include/TEACHER.h
+++ LIBRARY MANAGEMENT SYSTEM/include/TEACHER.h
@@ -6,17 +6,17 @@ class TEACHER : public READER {
     private:
         string facility;
     public:
-    TEACHER();
-    TEACHER(string, int, string, string, string, DATE, string, string, DATE, DATE, string);
-    void setFacility(string);
-    string getFacility();
-    void input();
-    void output();
-    void outputSetw();
-    void edit();
-    void readFileAReader (ifstream &inFile);
-    void exportFileAReader (ofstream &outFile);
-    ~TEACHER();
+        TEACHER();
+        TEACHER(string, int, string, string, string, DATE, string, string, DATE, DATE, string);
+        void setFacility(string);
+        string getFacility();
+        void input();
+        void output();
+        void outputSetw();
+        void edit();
+        void readFileAReader (ifstream &inFile);
+        void exportFileAReader (ofstream &outFile);
+        ~TEACHER();
 };
 
 #endif
