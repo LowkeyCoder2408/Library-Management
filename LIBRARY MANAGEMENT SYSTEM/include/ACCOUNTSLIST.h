d190bfae48a1d86c64fcc5cd84f7adfbc34d7add
Đỗ Kim Lâm <125809812+LowkeyCoder2408@users.noreply.github.com>
2023-06-04 19:10:58 +0700

    Update ACCOUNTSLIST.h

--- LIBRARY MANAGEMENT SYSTEM/include/ACCOUNTSLIST.h
+++ LIBRARY MANAGEMENT SYSTEM/include/ACCOUNTSLIST.h
@@ -1,25 +1,24 @@
-#ifndef ACCOUNTSLIST_H
-#define ACCOUNTSLIST_H
 #include "ACCOUNT.h"
 #include "ADMIN.h"
 #include "USER.h"
 #include <vector>
+#ifndef ACCOUNTSLIST_H
+#define ACCOUNTSLIST_H
 
-class ACCOUNTSLIST
-{
+class ACCOUNTSLIST {
     private:
-         vector<ACCOUNT*> accountsList;
+         vector <ACCOUNT*> accountsList;
     public:
         ACCOUNTSLIST();
-        void setAccountsList(vector<ACCOUNT*>);
-        vector<ACCOUNT*> getAccountsList();
+        void setAccountsList(vector <ACCOUNT*>);
+        vector <ACCOUNT*> getAccountsList();
         void inputAAcount(string);
         bool checkDuplicatedUsername(string);
         void readFile();
         void exportFile();
-        int checkAccount (string username, string password);
-        void deleteAccount (string);
+        int checkAccount(string username, string password);
+        void deleteAccount(string);
         ~ACCOUNTSLIST();
 };
 
-#endif // ACCOUNTSLIST_H
+#endif
